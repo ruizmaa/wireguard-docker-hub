9b901492c2347770e4a0196b10db3aa1d48615b5
Mario Cortés Ruiz <mario.cortes@meaningfuldata.eu>
2026-07-28 16:29:57 +0200

    fix: disble shellcheck SC2034 in colors.sh

--- scripts/lib/colors.sh
+++ scripts/lib/colors.sh
@@ -2,6 +2,7 @@
 # Shared ANSI color codes for terminal output. Not meant to be run directly.
 # Usage: source this file, then use $GREEN/$YELLOW/$RED/$CYAN/$NC in echo -e.
 
+# shellcheck disable=SC2034 # used by scripts that source this file
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 RED='\033[0;31m'
