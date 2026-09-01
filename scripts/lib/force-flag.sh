b63e551768cdf85e2cff3958496bfb809f8cc35b
Mario Cortés Ruiz <mario.cortes@meaningfuldata.eu>
2026-09-01 02:31:35 +0200

    fix shellcheck

--- scripts/lib/force-flag.sh
+++ scripts/lib/force-flag.sh
@@ -4,10 +4,12 @@
 
 # Only --force is supported, any other flag is a typo and should fail loudly
 parse_force_flag() {
+    # shellcheck disable=SC2034 # read by the script that sourced this file, not here
     FORCE="false"
     local arg
     for arg in "$@"; do
         case "$arg" in
+            # shellcheck disable=SC2034 # read by the script that sourced this file, not here
             --force) FORCE="true" ;;
             *) echo "Error: unknown flag '$arg'"; exit 1 ;;
         esac
